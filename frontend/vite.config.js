fb2e0108c787125d3ddcc1626b1cd2e5bf0fdc67
Debsankar Dhara <123552981+debsankardhara@users.noreply.github.com>
2026-08-28 15:46:08 +0530

    Restrict allowed hosts for Vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true // Allows all hosts, including your Render URL
-  }
+ server: {
+  allowedHosts: ['mern-frontend-latest-v3qs.onrender.com']
+}
 });
